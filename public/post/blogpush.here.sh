de2199a0c09ad11aa0fa040e623029fb22e919f4
unknown <710969718@qq.com>
2019-08-15 02:01:39 +0800

    autopush from blogpush

---
+++ public/post/blogpush.here.sh
@@ -0,0 +1,20 @@
+cd /d/Documents/RStudio/ccybnumath.github.io/
+cd ./content/post/
+find . -type f -name \*.md | grep -f old.file -v |xargs sed -i 's!(images!(/images!g'
+cd ..
+cd ..
+
+hugo
+git add .
+git commit -m "autopush from blogpush"
+git push
+cd public
+git add .
+git commit -m "autopush from blogpush"
+git push
+cd ..
+cd ./content/post/
+find . -type f -name \*.md > old.file
+cd ..
+cd ..
+
